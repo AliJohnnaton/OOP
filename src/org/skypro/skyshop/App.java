62976a9bddadc6b1ad6b703014357fa17eac7bde
AliJohnnaton <trofimov2002@mail.ru>
2025-04-01 22:06:50 +0300

    OOP3

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -1,10 +1,14 @@
 package org.skypro.skyshop;
 
-import org.skypro.skyshop.basket.ProductBasket;
+import org.skypro.skyshop.article.Article;
 import org.skypro.skyshop.product.DiscountedProduct;
 import org.skypro.skyshop.product.FixPriceProduct;
 import org.skypro.skyshop.product.Product;
 import org.skypro.skyshop.product.SimpleProduct;
+import org.skypro.skyshop.searchable.Searchable;
+import org.skypro.skyshop.searchengine.SearchEngine;
+
+import java.util.Arrays;
 
 public class App {
     public static void main(String[] args) {
@@ -14,30 +18,29 @@ public class App {
         Product cheese = new SimpleProduct("Сыр <<Liebendorf>>", 220);
         Product chocolate = new DiscountedProduct("Шоколад <<Россия - Щедрая душа>>", 80, 50);
         Product chips = new SimpleProduct("Чипсы <<Lay\\'s>>", 175);
+        Article sausageSecond = new Article("Колбаса <<Папа может>>", "Одна из тех колбас что мне нравиться");
+        Article sausageThird = new Article("Колбаса <<Дугушка>>", "Постоянно её беру");
 
-        ProductBasket basket = new ProductBasket();
-        basket.addProduct(bread);
-        basket.addProduct(milk);
-        basket.addProduct(sausage);
-        basket.addProduct(cheese);
-        basket.addProduct(chocolate);
-        basket.addProduct(chips);
+        SearchEngine searchables = new SearchEngine(10);
+        searchables.add(bread);
+        searchables.add(milk);
+        searchables.add(sausage);
+        searchables.add(cheese);
+        searchables.add(chocolate);
+        searchables.add(chips);
+        searchables.add(sausageSecond);
+        searchables.add(sausageThird);
 
-        basket.printBasket();
+        System.out.println(Arrays.toString(searchables.search("Колбаса")));
 
-        System.out.println(basket.getFullPrice() + "руб.");
-
-        System.out.println(basket.checkProductByName("Колбаса <<Вязанка>>"));
-
-        System.out.println(basket.checkProductByName("Колбаса <<Папа может>>"));
-
-        basket.clear();
-
-        basket.printBasket();
-
-        System.out.println(basket.getFullPrice() + "руб.");
-
-        System.out.println(basket.checkProductByName("Колбаса <<Вязанка>>"));
+        System.out.println();
+        Searchable[] searchingTmp;
+        searchingTmp = searchables.search("о");
+        for (Searchable s : searchingTmp) {
+            if (s != null) {
+                System.out.println(s.getStringRepresentation() + '\n');
+            }
+        }
 
     }
 }
